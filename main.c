0b96f3e2a27d662c671b25da08c6a66e9ade9abe
Kostiantyn <kostiantynmakhno@gmail.com>
2017-09-25 14:06:46 +0300

    has been corrected errors and has been  changed function get_value()

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-//#include <avr/io.h>
-//#include <util/delay.h>
-//#include <avr/interrupt.h>
 #include <stdio.h>
-
-void init_register(void);
 
 enum states {standby, code, vb, va, configuration, time, rd, power};
 enum signals {idle, pd6, pc5, pc3, pc4, pc6, pc7};
@@ -11,28 +6,28 @@ enum digit {dig0, dig1, dig2, dig3};
 enum value {null, one, two, three, four, five, six, seven, eight, nine};
 
 //volatile static uint8_t porthistory = 0xff;
-volatile unsigned int cnt3 = 0, cnt7 = 0, cnt4 = 0, cnt6 = 0;
+//volatile unsigned int cnt3 = 0, cnt7 = 0, cnt4 = 0, cnt6 = 0;
 
 unsigned char v[4];
-unsigned char* vptr = v;
+//unsigned char* vptr = v;
 
 enum states new_state(enum states, enum signals);
-/**************prototypes for forward value************************/
+//**************prototypes for forward value************************/
 enum value get_value(enum value);
 enum value get_value_for_hhour(enum value);
 enum value get_value_for_lhour(enum value);
 enum value get_value_for_hminutes(enum value);
-/******************************************************************/
+//******************************************************************/
 
-/****************prototypes for backforward value*****************/
+//****************prototypes for backforward value*****************/
 enum value get_bvalue(enum value);
 enum value get_bvalue_for_hhour(enum value);
 enum value get_bvalue_for_lhour(enum value);
 enum value get_bvalue_for_hminutes(enum value);
-/******************************************************************/
+//******************************************************************/
 typedef enum value (*funcv)(enum value);
 
-enum value get_new_value(enum value, enum signals, funcv);
+enum value get_new_value(enum value, enum signals, funcv, funcv);
 enum digit get_new_dig(enum states, enum signals, enum digit);
 
 enum states current_state = standby;
@@ -42,242 +37,13 @@ enum value current_value = null;
 
 funcv callback_get_value;
 
-/*const char dotsa = 0x01;
-const char dotsb = 0x02;
-const char dotsd = 0x04;
-char dotse = 0x08;
-const char dotnull = 0x00;
-const char dotsf = 0x10;
-const char dotsg = 0x20;
-const char dotsi = 0x40;*/
-
-/*void symcreate(char sym)
-{
-	switch(sym)
-	{
-		case '0':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-		case '1':
-			set_pin_low(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_low(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-		case '2':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_low(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_low(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '3':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_low(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '4':
-			set_pin_low(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '5':
-		case 'S':
-			set_pin_high(PORTA, PA5);
-			set_pin_low(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '6':
-			set_pin_high(PORTA, PA5);
-			set_pin_low(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '7':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_low(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-		case '8':
-		case 'B':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case '9':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case 'P':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_low(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case 'A':
-			set_pin_high(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_high(PORTC, PC2);
-			break;
-		case 'V':
-			set_pin_low(PORTA, PA5);
-			set_pin_high(PORTA, PA7);
-			set_pin_high(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-		case 'C':
-			set_pin_high(PORTA, PA5);
-			set_pin_low(PORTA, PA7);
-			set_pin_low(PORTC, PC0);
-			set_pin_high(PORTD, PD3);
-			set_pin_high(PORTB, PB1);
-			set_pin_high(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-		case 'N':
-		default:
-			set_pin_low(PORTA, PA5);
-			set_pin_low(PORTA, PA7);
-			set_pin_low(PORTC, PC0);
-			set_pin_low(PORTD, PD3);
-			set_pin_low(PORTB, PB1);
-			set_pin_low(PORTA, PA6);
-			set_pin_low(PORTC, PC2);
-			break;
-	}
-}
-
-void dotout(char dots)
-{
-        if(dots == 0)
-        {
-                set_pin_low(PORTC,LED_DOT1);
-                set_pin_low(PORTB,LED_DOT2);
-                set_pin_low(PORTD,LED_DOT3);
-                set_pin_low(PORTB,LED_DOT67);
-                set_pin_low(PORTB,LED_DOT8);
-                set_pin_low(PORTD,LED_DOT9);
-                set_pin_low(PORTB,LED_DOT11);
-
-                return;
-        }
-
-        if(dots&0x01) set_pin_high(PORTC,LED_DOT1); //D1_K
-        if(dots&0x02) set_pin_high(PORTB,LED_DOT2); //D2_K
-        if(dots&0x04) set_pin_high(PORTD,LED_DOT3); //D3_K
-        if(dots&0x08) set_pin_high(PORTB,LED_DOT67); //D6D7_K
-        if(dots&0x10) set_pin_high(PORTB,LED_DOT8); //D8_K
-        if(dots&0x20) set_pin_high(PORTD,LED_DOT9); //D9_K
-        if(dots&0x40) set_pin_high(PORTB,LED_DOT11); //D11_K
-
-}
-
-
-void showsym(char sym1, char sym2, char sym3, char sym4, const char dots)
-{
-	symcreate(sym1);
-
-	set_pin_high(PORTA, PA0);
-	_delay_us(1);
-	set_pin_low(PORTA, PA0);
-
-	symcreate(sym2);
-
-	set_pin_high(PORTA, PA1);
-	_delay_us(1);
-	set_pin_low(PORTA, PA1);
-
-	symcreate(sym3);
-
-	set_pin_high(PORTA, PA3);
-	_delay_us(1);
-	set_pin_low(PORTA, PA3);
-
-	symcreate(sym4);
-
-	set_pin_high(PORTA, PA4);
-	_delay_us(1);
-	set_pin_low(PORTA, PA4);
-
-        dotout(0);
-        dotout(dots);
-        set_pin_high(PORTA,LED_DOT_COM);
-        _delay_us(1);
-        set_pin_low(PORTA,LED_DOT_COM);
-
-}*/
-
-/*void modeHandler()
-{
-	if((PIND&(1 << PD6)) == 0)
-	{
-		_delay_ms(40);
-		if((PIND&(1 << PD6)))
-			signal = pd6;
-	}
-}*/
-
-
-int main()
-{
-	//init_register();
-	callback_get_value = get_value;
-	//sei();
+int main(int argc, char **argv) {
 
 	while(1)
 	{
-		current_value = get_new_value(current_value, signal, callback_get_value);
 
 	}
+	return 0;
 }
 
 enum states new_state(enum states current_state, enum signals sgn)
@@ -575,29 +341,25 @@ enum digit get_new_dig(enum states st, enum signals sgn, enum digit current_digi
 	return d;
 }
 
-enum value get_new_value(enum value current_value, enum signals sg, enum value (*f)(enum value v))
+enum value get_new_value(enum value current_value, enum signals sg, funcv forward, funcv backforward)
 {
 	static enum value nwvl;
 	switch(sg)
 	{
 		case pc4:
-			nwvl = f(current_value);
-			signal = idle;
+			nwvl = forward(current_value);
 		break;
 
 		case pc6:
-			nwvl = nine - f(current_value);
-			signal = idle;
+			nwvl = backforward(current_value);
 		break;
 
 		default:
-			signal = idle;
+			nwvl = current_value;
 		break;
 	}
 	return nwvl;
 }
-
-
 
 enum value get_value(enum value current_value)
 {
@@ -647,52 +409,50 @@ enum value get_value(enum value current_value)
 	return new_value;
 }
 
-
-
-/*void init_register(void)
+enum value get_bvalue(enum value current_value)
 {
-	DDRB |= 0x1A;
-	DDRD |= 0x18;
-	PORTC |= ((1 << PC6)|(1 << PC4)|(1 << PC5)|(1 << PC7)|(1 << PC3));
-	DDRC |= 0x07;
-
-	PORTD |= (1 << PD6);
-
-	DDRA |= 0xff;
-	GICR |= (1 << PCIE1);
-	PCMSK1 |= ((1 << PCINT11)|(1 << PCINT12)|(1 << PCINT14)|(1 << PCINT15)|(1 << PCINT13));
-}*/
-
-/*ISR(PCINT1_vect)
-{
-	uint8_t changebits;
-	changebits = PINC ^ porthistory;
-	porthistory = PINC;
-
-	if((changebits&(1 << PINC3)))
+	static enum value new_value = 0;
+	switch(current_value)
 	{
-		cnt3++;
-		if((cnt3%2 == 0) && (cnt3 != 0)) signal = pc3;
-	}
+		case null:
+			new_value = nine;
+		break;
 
-	if((changebits&(1 << PINC7)))
-	{
-		cnt7++;
-		if((cnt7%2 == 0) && (cnt7 != 0)) signal = pc7;
-	}
+		case one:
+			new_value = null;
+		break;
 
-	if((changebits&(1 << PINC4)))
-	{
-		signal = pc4;
-	}
+		case two:
+			new_value = one;
+		break;
 
-	if((changebits&(1 << PINC6)))
-	{
-		signal = pc6;
-	}
+		case three:
+			new_value = two;
+		break;
 
-	if((changebits&(1 << PINC5)))
-	{
-		signal = pc5;
+		case four:
+			new_value = three;
+		break;
+
+		case five:
+			new_value = four;
+		break;
+
+		case six:
+			new_value = five;
+		break;
+
+		case seven:
+			new_value = six;
+		break;
+
+		case eight:
+			new_value = seven;
+		break;
+
+		case nine:
+			new_value = eight;
+		break;
 	}
-}*/
+	return new_value;
+}
