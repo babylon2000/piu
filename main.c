c6880a57bdf97a47198d41993de07e1d38aaf7dc
Kostiantyn <kostiantynmakhno@gmail.com>
2017-09-28 18:20:07 +0300

    added new function of handle pc5

--- main.c
+++ main.c
@@ -5,17 +5,23 @@
 #include <stdlib.h>
 
 #define LEN	4
+#define MYUBRR 95
+
+#define blink_on(port, pin)	port |= pin
+#define blink_off(port, pin) port &=~ pin
 
 volatile static uint8_t porthistory = 0xff;
-volatile unsigned int cnt3 = 0, cnt7 = 0, cnt4 = 0, cnt6 = 0;
+volatile unsigned int cnt3 = 0, cnt7 = 0, cnt4 = 0, cnt6 = 0, cnt5 = 0;
 
 #include "include/crc8.h"
 #include "include/prototype.h"
 #include "include/frame_creator.h"
 #include "include/showsym.h"
+#include "include/uart.h"
 
 void init_register(void);
 void modeHandler(void);
+void btn_pc5_handler(enum states, enum signals);
 
 enum states current_state = standby;
 enum signals signal = idle;
@@ -23,18 +29,23 @@ enum digit dig = dig1;
 enum value current_value = null;
 
 unsigned char v[4];
+unsigned char* vptr = v;
+char dots = 0x00;
 
 int main(int argc, char **argv) {
 
 	init_register();
+	uart_init(MYUBRR);
 	sei();
 	while(1)
 	{
-		showsym(v, 0x00);
+		showsym(v, dots);
 		modeHandler();
 		current_state = new_state(current_state, signal);
+		//if(signal == pc5) current_state = code;
 		dig = get_new_dig(current_state, signal, dig);
 		set_value(v, current_state, dig, get_new_value);
+		btn_pc5_handler(current_state, signal);
 		signal = idle;
 	}
 	return 0;
@@ -57,6 +68,23 @@ void modeHandler()
 	}
 }
 
+void btn_pc5_handler(enum states st, enum signals sg)
+{
+	switch (sg) {
+		case pc5:
+			if((st != standby) && (st != vb))
+			{
+				unsigned char* new_frame = frame_creator(v, st, crc8);
+				uart_send_package(new_frame, sizeof(new_frame)/sizeof(new_frame[0]));
+				dots = 0x02;
+				free(new_frame);//пока не определюсь с проверкой!
+			}
+			break;
+		default:
+			break;
+	}
+}
+
 void init_register(void)
 {
 	DDRB |= 0x1A;
@@ -71,7 +99,7 @@ void init_register(void)
 	PCMSK1 |= ((1 << PCINT11)|(1 << PCINT12)|(1 << PCINT14)|(1 << PCINT15)|(1 << PCINT13));
 }
 
-/**********interrupt handler**********************************/
+/**********interrupt handler for pin change**********************************/
 ISR(PCINT1_vect)
 {
 	uint8_t changebits;
@@ -104,7 +132,8 @@ ISR(PCINT1_vect)
 
 	if((changebits&(1 << PINC5)))
 	{
-		signal = pc5;
+		cnt5++;
+		if((cnt5%2 == 0) && (cnt5 != 0)) signal = pc5;
 	}
 }
 /**********************************************************************/
