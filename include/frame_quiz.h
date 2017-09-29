6899636eaa29e53aeee5a2881a7596cbbf5d68e9
Kostiantyn <kostiantynmakhno@gmail.com>
2017-09-29 17:07:34 +0300

    prototype of quiz creator

---
+++ include/frame_quiz.h
@@ -0,0 +1,13 @@
+/*
+ * frame_quiz.h
+ *
+ *  Created on: 29 сент. 2017
+ *      Author: kostiantyn
+ */
+
+#ifndef FRAME_QUIZ_H_
+#define FRAME_QUIZ_H_
+
+unsigned char* frame_quiz();
+
+#endif /* FRAME_QUIZ_H_ */
